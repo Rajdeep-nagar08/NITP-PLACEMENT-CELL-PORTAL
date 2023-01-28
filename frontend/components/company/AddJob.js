d90fab83dc59eef429142555d3c3f288500d5720
Ayushi Gupta <guptayushi001@gmail.com>
2023-01-28 20:22:27 +0530

    company ui updated

--- frontend/components/company/AddJob.js
+++ frontend/components/company/AddJob.js
@@ -457,6 +457,14 @@ export default function AddJob({ token = '' }) {
           >
             Add
           </button>
+          <button>
+            <a
+            onClick={() => logout()}
+            className='ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
+                  >
+                    Logout
+                  </a>
+            </button>
         </div>
       </div>
     </form>
--- frontend/components/company/Sidebar.js
+++ frontend/components/company/Sidebar.js
@@ -118,38 +118,8 @@ export default function Sidebar({ heading = '', children }) {
                 </div>
               </Transition.Child>
               <div className='flex-1 h-0 pt-5 pb-4 overflow-y-auto'>
-                <div className='flex-shrink-0 flex items-center px-4'>
-                  <Image
-                    width={100}
-                    height={40}
-                    alt='Unirchitect'
-                    src='/images/iitp.svg'
-                  />
-                </div>
-                <nav className='mt-5 px-2 space-y-1'>
-                  {navigation.map((item) => (
-                    <Link href={item.href} key={item.name}>
-                      <a
-                        className={classNames(
-                          item.href === router.pathname
-                            ? 'bg-gray-900 text-white'
-                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                          'group flex items-center px-2 py-2 text-base font-medium rounded-md'
-                        )}
-                      >
-                        <item.icon
-                          className={classNames(
-                            item.href === router.pathname
-                              ? 'text-gray-300'
-                              : 'text-gray-400 group-hover:text-gray-300',
-                            'mr-4 flex-shrink-0 h-6 w-6'
-                          )}
-                          aria-hidden='true'
-                        />
-                        {item.name}
-                      </a>
-                    </Link>
-                  ))}
+                <nav className='mt-5 px-2 space-x-1'>
+                      
                   <a
                     onClick={() => logout()}
                     className={classNames(
@@ -202,87 +172,6 @@ export default function Sidebar({ heading = '', children }) {
       </Transition.Root>
 
       {/* Static sidebar for desktop */}
-      <div className='hidden md:flex md:w-64 md:flex-col md:fixed md:inset-y-0'>
-        {/* Sidebar component, swap this element with another sidebar if you like */}
-        <div className='flex-1 flex flex-col min-h-0 bg-cyan-800'>
-          <div className='flex-1 flex flex-col pt-5 pb-4 overflow-y-auto'>
-            <div className='flex items-center flex-shrink-0 px-4'>
-              <Image
-                width={180}
-                height={100}
-                alt='IIT Patna'
-                src='/images/iitp.svg'
-              />
-            </div>
-            <nav className='mt-5 flex-1 px-2 space-y-1'>
-              {navigation.map((item) => (
-                <Link key={item.name} href={item.href}>
-                  <a
-                    className={classNames(
-                      item.href === router.pathname
-                        ? 'bg-gray-900 text-white'
-                        : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                      'group flex items-center px-2 py-2 text-sm font-medium rounded-md'
-                    )}
-                  >
-                    <item.icon
-                      className={classNames(
-                        item.href === router.pathname
-                          ? 'text-gray-300'
-                          : 'text-gray-400 group-hover:text-gray-300',
-                        'mr-3 flex-shrink-0 h-6 w-6'
-                      )}
-                      aria-hidden='true'
-                    />
-                    {item.name}
-                  </a>
-                </Link>
-              ))}
-              <a
-                onClick={() => logout()}
-                className={classNames(
-                  false
-                    ? 'bg-gray-900 text-white'
-                    : 'text-gray-300 hover:bg-gray-700 hover:text-white',
-                  'group flex items-center px-2 py-2 text-sm font-medium rounded-md cursor-pointer'
-                )}
-              >
-                <LogoutIcon
-                  className={classNames(
-                    false
-                      ? 'text-gray-300'
-                      : 'text-gray-400 group-hover:text-gray-300',
-                    'mr-3 flex-shrink-0 h-6 w-6'
-                  )}
-                  aria-hidden='true'
-                />
-                Logout
-              </a>
-            </nav>
-          </div>
-          <div className='flex-shrink-0 flex bg-gray-700 p-4'>
-            <a href='#' className='flex-shrink-0 w-full group block'>
-              <div className='flex items-center'>
-                <div>
-                  <img
-                    className='inline-block h-9 w-9 rounded-full'
-                    src='https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80'
-                    alt=''
-                  />
-                </div>
-                <div className='ml-3'>
-                  <p className='text-sm font-medium text-white'>
-                    {user && user.email}
-                  </p>
-                  <p className='text-xs font-medium text-gray-300 group-hover:text-gray-200 uppercase'>
-                    {user && user.username}
-                  </p>
-                </div>
-              </div>
-            </a>
-          </div>
-        </div>
-      </div>
       <div className='md:pl-64 flex flex-col flex-1'>
         <div className='sticky top-0 z-10 md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3 bg-gray-100'>
           <button
@@ -303,6 +192,7 @@ export default function Sidebar({ heading = '', children }) {
             </div>
             <div className='max-w-7xl mx-auto px-4 sm:px-6 md:px-8'>
               {children}
+            
             </div>
           </div>
         </main>
