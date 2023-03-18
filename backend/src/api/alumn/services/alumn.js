7d701db9c0192dedcc5b2bbdaaac2793bff7032f
ayushi1881 <guptayushi001@gmail.com>
2023-03-18 19:13:16 +0530

    .

--- backend/src/api/alumn/services/alumn.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * alumn service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::alumn.alumn');
--- backend/src/api/alumni/services/alumni.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * alumni service.
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::alumni.alumni');
